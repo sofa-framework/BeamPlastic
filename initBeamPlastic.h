99e1ee2ea975be200d56d94c13e7bd3beeeba6b2
Camille <camille.krewcun@uca.fr>
2020-11-23 10:39:14 +0100

    [BeamPlastic] CHANGE plugin name
    Files implementing the BeamPlasticFEMForceField component were initially
    extracted from a different plugin dedicated to stent expansion. To keep
    the corresponding Git history, we kept the original 'StentExp' plugin
    name in previous commits.
    Here, we change the plugin name to BeamPlastic and modify the source
    files and CMakeLists files accordingly. No functional change is made
    regarding the code of the BeamPlasticFEMForceField component with this
    commit.

---
+++ initBeamPlastic.h
@@ -0,0 +1,16 @@
+#ifndef BEAMPLASTIC_H
+#define BEAMPLASTIC_H
+
+#include <sofa/helper/system/config.h>
+
+#ifdef SOFA_BUILD_BEAMPLASTIC
+#define SOFA_BeamPlastic_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#else
+#define SOFA_BeamPlastic_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#endif
+
+/**
+TODO: Main page of the doxygen documentation for the plugin
+*/
+
+#endif
